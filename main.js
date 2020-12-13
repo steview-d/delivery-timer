3fbd7ba3da39470a3bc473500d62e9572e6af8a7
Dave Laffan <dave.laffan@outlook.com>
2020-12-13 18:31:58 +0000

    Add countdown timer

--- main.js
+++ main.js
@@ -1,7 +1,44 @@
-console.log("fdfd");
+function getTimeRemaining(endtime) {
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
 
-var today = new Date();
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds,
+  };
+}
 
-console.log(today.getDate());
-console.log(today.getHours());
-console.log(today.getDay());
+function initializeClock(id, endtime) {
+  const clock = document.getElementById(id);
+  const daysSpan = clock.querySelector('.days');
+  const hoursSpan = clock.querySelector('.hours');
+  const minutesSpan = clock.querySelector('.minutes');
+  const secondsSpan = clock.querySelector('.seconds');
+
+  function updateCountdown() {
+    const t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateCountdown();
+  const timeinterval = setInterval(updateCountdown, 1000);
+}
+
+const cutoffTimeString = '2020-12-14 15:00:00:700';
+const cutoffTime = new Date(cutoffTimeString.replace(/-/g, '/'))
+console.log(cutoffTime);
+initializeClock('countdown', cutoffTime);
