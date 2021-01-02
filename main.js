32682e59a45fe5fdbd88deebf43e5a313576096d
Dave Laffan <dave.laffan@outlook.com>
2021-01-02 18:00:31 +0000

    Update to avoid weekends and flag if specific day

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-function getTimeRemaining(endtime) {
-  const total = Date.parse(endtime) - Date.parse(new Date());
+const leadTime = 1;
+const now = new Date();
+const next = new Date();
+
+// Daily cut off time
+const cutoffTimeHours = 17; // The hour, in 24HR clock format
+const cutoffTimeMinutes = 0; // Minutes - value between 0 and 59
+
+// set 'next' var time to cutoff time
+next.setHours(cutoffTimeHours);
+next.setMinutes(cutoffTimeMinutes);
+next.setSeconds(0);
+
+/**
+ * @param {*} cutoffTime - Calculate and return the time remaining between 'cutoffTime' and now.
+ */
+function getTimeRemaining(cutoffTime) {
+  const total = Date.parse(cutoffTime) - Date.parse(new Date());
   const seconds = Math.floor((total / 1000) % 60);
   const minutes = Math.floor((total / 1000 / 60) % 60);
   const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
@@ -14,7 +30,12 @@ function getTimeRemaining(endtime) {
   };
 }
 
-function initializeClock(id, endtime) {
+/**
+ * Set up the clock
+ * @param {*} id - The ID of the element to update
+ * @param {*} cutoffTime - The future date time to count down towards
+ */
+function initializeClock(id, cutoffTime) {
   const clock = document.getElementById(id);
   const daysSpan = clock.querySelector('.days');
   const hoursSpan = clock.querySelector('.hours');
@@ -22,7 +43,7 @@ function initializeClock(id, endtime) {
   const secondsSpan = clock.querySelector('.seconds');
 
   function updateCountdown() {
-    const t = getTimeRemaining(endtime);
+    const t = getTimeRemaining(cutoffTime);
 
     daysSpan.innerHTML = t.days;
     hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
@@ -34,43 +55,53 @@ function initializeClock(id, endtime) {
     }
   }
 
-  updateCountdown();
   const timeinterval = setInterval(updateCountdown, 1000);
+  updateCountdown();
 }
 
-const now = new Date ()
-const next = new Date()
-// Find a way of getting to NEXT 3pm, be it that day or next
+/**
+ * Convert a new Date() object to an ISO String formatted date
+ * @param {object} dateObject
+ * @param {bool} time - Include time, or not
+ */
+function convertDateTimeToIsoString(dateObject, time=false) {
+  let timeStr = (time==true) ? -9:10;
+  return dateObject.toISOString().replace('T', ' ').slice(0, timeStr);
+}
 
-// NEXT 3pm
-next.setHours(15);
-next.setMinutes(0);
-next.setSeconds(0);
+
+// if cutoff time has passed for today, set cutoff day to tomorrow
 if (now - next > 0) {
-    next.setDate(next.getDate() +1);
+  next.setDate(next.getDate() + 1);
 }
 
-// THEN check if it's a weekend
-if (next.getDay() == 0) {
-    next.setDate(next.getDate() +1)
-}
+// If a weekend day, set date to following Monday
 if (next.getDay() == 6) {
-    next.setDate(next.getDate() +2)
+  // If it's Saturday, add 1 day
+  next.setDate(next.getDate() + 2);
+}
+if (next.getDay() == 0) {
+  // If it's Sunday, add 1 day
+  next.setDate(next.getDate() + 1);
 }
 
 // THEN check if it's a date that should be skipped
-datesToAvoid = [
-    '2020-12-25', '2021-01-01'
-]
-console.log("-----------")
-console.log(next)
-console.log(next.toISOString().replace('T', ' ').slice(0, -5));
-console.log('2020-12-14 15:00:00')
-console.log("-----------")
+nonDespatchDay = ['2020-12-25', '2021-01-04'];
 
-const cutoffTimeString = '2020-12-14 15:00:00:700';
-// const cutoffTime = new Date(cutoffTimeString.replace(/-/g, '/'))
-const cutoffTime = new Date(cutoffTimeString)
+if (nonDespatchDay.includes(convertDateTimeToIsoString(next))) {
+  console.log('FOUND A DATE TO AVOID!!!');
+}
 
-console.log(cutoffTime);
-initializeClock('countdown', cutoffTime);
+
+
+
+const nextCutoff = convertDateTimeToIsoString(next, true);
+
+initializeClock('countdown', nextCutoff);
+
+
+// Debugging //
+console.log('-----------');
+console.log(next);
+console.log(nextCutoff);
+console.log('-----------');
